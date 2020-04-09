b9b081637e22c8067a0cb5ec96e35703175c4729
Michael Zapata <mikeyfriends@hotmail.com>
2020-04-09 15:53:34 +0100

    added media to settings

--- RealState/settings.py
+++ RealState/settings.py
@@ -135,3 +135,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'RealState/static')
 ]
+
+# Images|media added during runtime.
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
