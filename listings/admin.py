b52618d35f33189c9353de50b7f654745bcbb5a4
Michael Zapata <mikeyfriends@hotmail.com>
2020-04-09 19:43:36 +0100

    added llistings and admin customization

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Listing
+
+class ListingAdmin(admin.ModelAdmin):
+    #Fields we want to show in the Listing
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title') # clickable element to access object
+    list_filter = ('realtor',) #Filter box to filter by Realtor
+    list_editable = ('is_published',) # Edit field on the Table Row itself
+    #Fields we'll use to search in the search box
+    search_fields = ('title', 'description', 'address', 'city', 'state', 'zipcode', 'price')
+    list_per_page = 25
+
 
 # Register your models here.
+admin.site.register(Listing, ListingAdmin)
--- listings/views.py
+++ listings/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Listing
 # Create your views here.
 def index(request):
-    return render(request, 'listings.html')
+        # get an object using its id ALTERNATIVE TO:     product = get_object_or_404(Product, pk=id)
+    try:
+        allListings = Listing.objects.all()
+    except Exception as anyException:
+        return render(request,'listings.html', {'error':anyException})
+    #else will only run if try was successful
+    else:
+        return render(request,'listings.html', {'listings':allListings})
 
 def listing(request, listing_id):
     return render(request, 'listing.html')
