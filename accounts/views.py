e13ef1d8e87644c2439d10fc60d7e6fa9329d2f9
Michael Zapata <mikeyfriends@hotmail.com>
2020-04-09 14:18:15 +0100

    added accounts interfaces

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
-# Create your views here.
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def logout(request):
+    return redirect('index')
+
+def dashboard(request):
+
+  return render(request, 'dashboard.html', {})
